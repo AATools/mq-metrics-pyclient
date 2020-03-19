175e69494f3cf17e6b1b030470df96f859f715e8
woblerr <antkurochkin@gmail.com>
2020-03-19 20:17:13 +0300

    Updated current tests for #2

--- tests/test_mq_channels.py
+++ tests/test_mq_channels.py
@@ -36,9 +36,20 @@ class TestExtractChannelName(unittest.TestCase):
 
 class TestGetTemplate(unittest.TestCase):
     def test_get_template(self):
-        tempalte = {'CHANNEL': '',
+        tempalte = {'BATCHES': '',
+                    'BUFSRCVD': '',
+                    'BUFSSENT': '',
+                    'BYTSRCVD': '',
+                    'BYTSSENT': '',
+                    'CHANNEL': '',
                     'CHLTYPE': '',
+                    'CHSTADA': '',
+                    'CHSTATI': '',
                     'CONNAME': '',
+                    'JOBNAME': '',
+                    'LSTMSGDA': '',
+                    'LSTMSGTI': '',
+                    'MSGS': '',
                     'RQMNAME': '',
                     'XMITQ': '',
                     'SUBSTATE': '',
@@ -49,30 +60,51 @@ class TestGetTemplate(unittest.TestCase):
 
 class TestGetChannelStatus(unittest.TestCase):
     def test_get_channel_status(self):
-        input_data = '''5724-H72 (C) Copyright IBM Corp. 1994, 2011.  ALL RIGHTS RESERVED.
-Starting MQSC for queue manager TEST.\n\n\n     1 : dis chstatus(ADMIN.SVRCONN)
-AMQ8417: Display Channel Status details.\n   CHANNEL(ADMIN.SVRCONN)                 CHLTYPE(SVRCONN)\n   CONNAME(127.0.0.1)                      CURRENT\n   STATUS(RUNNING)                         SUBSTATE(RECEIVE)
+        input_data = '''5724-H72 (C) Copyright IBM Corp. 1994, 2011. ALL RIGHTS RESERVED.
+Starting MQSC for queue manager TEST.\n\n\n     1 : dis chstatus(TEST.SRVCONN) BATCHES BUFSRCVD BUFSSENT BYTSRCVD BYTSSENT CHSTADA CHSTATI JOBNAME LSTMSGDA LSTMSGTI MSGS
+AMQ8417: Display Channel Status details.\n   CHANNEL(ADMIN.SVRCONN)                 CHLTYPE(SVRCONN)\n   BUFSRCVD(7216)                          BUFSSENT(7215)\n   BYTSRCVD(4894300)                       BYTSSENT(949752)\n   CHSTADA(2020-03-19)                     CHSTATI(18.00.00)\n   CONNAME(127.0.0.1)                      CURRENT\n   JOBNAME(000010EC00000007)               LSTMSGDA(2020-03-19)\n   LSTMSGTI(18.15.01)                      MSGS(1510)\n   STATUS(RUNNING)                         SUBSTATE(RECEIVE)
 One MQSC command read.\nNo commands have a syntax error.\nAll valid MQSC commands were processed.\n'''
-        lable_data = [{'CHANNEL': 'ADMIN.SVRCONN',
+        label_data = [{'BUFSRCVD': '7216',
+                       'BUFSSENT': '7215',
+                       'BYTSRCVD': '4894300',
+                       'BYTSSENT': '949752',
+                       'CHANNEL': 'ADMIN.SVRCONN',
                        'CHLTYPE': 'SVRCONN',
+                       'CHSTADA': '2020-03-19',
+                       'CHSTATI': '18.00.00',
                        'CONNAME': '127.0.0.1',
                        'CURRENT': ' ',
+                       'JOBNAME': '000010EC00000007',
+                       'LSTMSGDA': '2020-03-19',
+                       'LSTMSGTI': '18.15.01',
+                       'MSGS': '1510',
                        'STATUS': 'RUNNING',
                        'SUBSTATE': 'RECEIVE'}]
-        check_data = [{'CHANNEL': 'ADMIN.SVRCONN',
+        check_data = [{'BATCHES': '',
+                       'BUFSRCVD': '7216',
+                       'BUFSSENT': '7215',
+                       'BYTSRCVD': '4894300',
+                       'BYTSSENT': '949752',
+                       'CHANNEL': 'ADMIN.SVRCONN',
                        'CHLTYPE': 'SVRCONN',
+                       'CHSTADA': '2020-03-19',
+                       'CHSTATI': '18.00.00',
                        'CONNAME': '127.0.0.1',
+                       'JOBNAME': '000010EC00000007',
+                       'LSTMSGDA': '2020-03-19',
+                       'LSTMSGTI': '18.15.01',
+                       'MSGS': '1510',
                        'RQMNAME': '',
                        'STATUS': 'RUNNING',
                        'SUBSTATE': 'RECEIVE',
                        'XMITQ': ''}]
-        self.assertEqual(check_data, get_channel_status(input_data, lable_data))
+        self.assertEqual(check_data, get_channel_status(input_data, label_data))
 
     def test_get_channel_status_not_found(self):
         input_data = '''5724-H72 (C) Copyright IBM Corp. 1994, 2011.  ALL RIGHTS RESERVED.
 Starting MQSC for queue manager TEST.\n\n\n     1 : dis chstatus(TEST.SRVCONN)
 AMQ8420: Channel Status not found.\nOne MQSC command read.\nNo commands have a syntax error.\nOne valid MQSC command could not be processed.\n'''
-        lable_data = [{'ALTDATE': '2017-05-02',
+        label_data = [{'ALTDATE': '2017-05-02',
                        'ALTTIME': '11.22.47',
                        'CHANNEL': 'TEST.SVRCONN',
                        'CHLTYPE': 'SVRCONN',
@@ -98,14 +130,25 @@ AMQ8420: Channel Status not found.\nOne MQSC command read.\nNo commands have a s
                        'SSLCIPH': ' ',
                        'SSLPEER': ' ',
                        'TRPTYPE': 'TCP'}]
-        check_data = [{'CHANNEL': 'TEST.SVRCONN',
-                       'CHLTYPE': 'SVRCONN',
-                       'CONNAME': '',
-                       'RQMNAME': '',
-                       'STATUS': '',
-                       'SUBSTATE': '',
-                       'XMITQ': ''}]
-        self.assertEqual(check_data, get_channel_status(input_data, lable_data))
+        check_data = [{ 'BATCHES': '',
+                        'BUFSRCVD': '',
+                        'BUFSSENT': '',
+                        'BYTSRCVD': '',
+                        'BYTSSENT': '',
+                        'CHANNEL': 'TEST.SVRCONN',
+                        'CHLTYPE': 'SVRCONN',
+                        'CHSTADA': '',
+                        'CHSTATI': '',
+                        'CONNAME': '',
+                        'JOBNAME': '',
+                        'LSTMSGDA': '',
+                        'LSTMSGTI': '',
+                        'MSGS': '',
+                        'RQMNAME': '',
+                        'XMITQ': '',
+                        'SUBSTATE': '',
+                        'STATUS': ''}]
+        self.assertEqual(check_data, get_channel_status(input_data, label_data))
 
 
 class TestFormatChannelOutput(unittest.TestCase):
@@ -159,43 +202,29 @@ One command read.\nNo commands have a syntax error.\nAll valid MQSC commands wer
 
 
 class TestMakeMetricForMqChannelsStatus(unittest.TestCase):
-    def test_make_metric_connection_count(self):
-        channel_data = {'CHANNEL': 'ADMIN.SVRCONN',
-                        'CHLTYPE': 'SVRCONN',
-                        'CONNAME': '127.0.0.1',
-                        'RQMNAME': '',
-                        'STATUS': 'RUNNING',
-                        'SUBSTATE': 'RECEIVE',
-                        'XMITQ': ''
-                        }
-        mq_manager = 'TEST'
-        metric_type = 'connection_count'
-        conn_count = 2
-        check_data = 'mq_channel_connection_count{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", rqmname="", channel="ADMIN.SVRCONN"} 2\n'
-        self.assertEqual(check_data, make_metric_for_mq_channels_status(channel_data, mq_manager, metric_type, conn_count))
-
+    mq_manager = 'TEST'
+    channel_data = {'BATCHES': '',
+                    'BUFSRCVD': '7216',
+                    'BUFSSENT': '7215',
+                    'BYTSRCVD': '4894300',
+                    'BYTSSENT': '949752',
+                    'CHANNEL': 'ADMIN.SVRCONN',
+                    'CHLTYPE': 'SVRCONN',
+                    'CHSTADA': '2020-03-19',
+                    'CHSTATI': '18.00.00',
+                    'CONNAME': '127.0.0.1',
+                    'JOBNAME': '000010EC00000007',
+                    'LSTMSGDA': '2020-03-19',
+                    'LSTMSGTI': '18.15.01',
+                    'MSGS': '1510',
+                    'RQMNAME': '',
+                    'STATUS': 'RUNNING',
+                    'SUBSTATE': 'RECEIVE',
+                    'XMITQ': ''}
     def test_make_metric_status(self):
-        status_dict = {
-                     'STARTING': 2,
-                     'RUNNING': 3,
-                     'STOPPED': 6,
-                     '': 0,
-                     }
-        for status in status_dict:
-            status_name = status
-            status_code = status_dict[status]
-            channel_data = {'CHANNEL': 'ADMIN.SVRCONN',
-                            'CHLTYPE': 'SVRCONN',
-                            'CONNAME': '127.0.0.1',
-                            'RQMNAME': '',
-                            'STATUS': status_name,
-                            'SUBSTATE': 'RECEIVE',
-                            'XMITQ': ''
-                            }
-            mq_manager = 'TEST'
             metric_type = 'status'
-            check_data = 'mq_channel_status{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", rqmname="", channel="ADMIN.SVRCONN"} %s\n' % (status_code)
-            self.assertEqual(check_data, make_metric_for_mq_channels_status(channel_data, mq_manager, metric_type))
+            check_data = 'mq_channel_status{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 3\n'
+            self.assertEqual(check_data, make_metric_for_mq_channels_status(self.channel_data, self.mq_manager, metric_type))
 
 
 if __name__ == '__main__':
--- tests/test_mq_metrics_client.py
+++ tests/test_mq_metrics_client.py
@@ -9,27 +9,60 @@ sys.path.append(os.getcwd())
 class TestGetMqChannels(unittest.TestCase):
     def test_get_mq_channels_metrics(self):
         mqm = 'TEST'
-        input_data = [[({'CHANNEL': 'TEST',
-                         'CHLTYPE': 'SVR',
-                         'CONNAME': '10.92.10.10(1414)',
-                         'RQMNAME': 'TEST',
-                         'STATUS': 'RUNNING',
-                         'SUBSTATE': 'MQGET',
-                         'XMITQ': 'TEST'},
-                        1)],
-                      [({'CHANNEL': 'ADMIN.SVRCONN',
-                         'CHLTYPE': 'SVRCONN',
-                         'CONNAME': '127.0.0.1',
-                         'RQMNAME': '',
-                         'STATUS': 'RUNNING',
-                         'SUBSTATE': 'RECEIVE',
-                         'XMITQ': ''},
-                        5)]]
-        check_data = '''mq_channel_connection_count{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", rqmname="TEST", channel="TEST"} 1
-mq_channel_status{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", rqmname="TEST", channel="TEST"} 3
-mq_channel_connection_count{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", rqmname="", channel="ADMIN.SVRCONN"} 5
-mq_channel_status{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", rqmname="", channel="ADMIN.SVRCONN"} 3\n'''
+        input_data = {'ADMIN.SRVCONN':[{'BATCHES': '50',
+                                         'BUFSRCVD': '1000',
+                                         'BUFSSENT': '1100',
+                                         'BYTSRCVD': '4894300',
+                                         'BYTSSENT': '949752',
+                                         'CHANNEL': 'TEST',
+                                         'CHLTYPE': 'SVR',
+                                         'CHSTADA': '2020-03-19',
+                                         'CHSTATI': '17.00.00',
+                                         'CONNAME': '10.92.10.10(1414)',
+                                         'JOBNAME': '00005C6800000001',
+                                         'LSTMSGDA': '2020-03-19',
+                                         'LSTMSGTI': '18.30.01',
+                                         'MSGS': '50',
+                                         'RQMNAME': 'TEST',
+                                         'STATUS': 'RUNNING',
+                                         'SUBSTATE': 'MQGET',
+                                         'XMITQ': 'TEST'},
+                                       {'BATCHES': '',
+                                         'BUFSRCVD': '7216',
+                                         'BUFSSENT': '7215',
+                                         'BYTSRCVD': '4894300',
+                                         'BYTSSENT': '949752',
+                                         'CHANNEL': 'ADMIN.SVRCONN',
+                                         'CHLTYPE': 'SVRCONN',
+                                         'CHSTADA': '2020-03-19',
+                                         'CHSTATI': '18.00.00',
+                                         'CONNAME': '127.0.0.1',
+                                         'JOBNAME': '000010EC00000007',
+                                         'LSTMSGDA': '2020-03-19',
+                                         'LSTMSGTI': '18.15.01',
+                                         'MSGS': '1510',
+                                         'RQMNAME': '',
+                                         'STATUS': 'RUNNING',
+                                         'SUBSTATE': 'RECEIVE',
+                                         'XMITQ': ''}]}
+        check_data = '''mq_channel_status{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", jobname="00005C6800000001", channel="TEST"} 3
+mq_channel_buffers{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", indicator="buffers_received", jobname="00005C6800000001", channel="TEST"} 1000
+mq_channel_buffers{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", indicator="buffers_sent", jobname="00005C6800000001", channel="TEST"} 1100
+mq_channel_bytes{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", indicator="bytes_received", jobname="00005C6800000001", channel="TEST"} 4894300
+mq_channel_bytes{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", indicator="bytes_sent", jobname="00005C6800000001", channel="TEST"} 949752
+mq_channel_lmsg{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", jobname="00005C6800000001", channel="TEST"} 1584631801
+mq_channel_msgs{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", jobname="00005C6800000001", channel="TEST"} 50
+mq_channel_batches{qmname="TEST", conname="10.92.10.10(1414)", substate="MQGET", xmitq="TEST", chltype="SVR", chstada="2020-03-19", chstati="17.00.00", rqmname="TEST", jobname="00005C6800000001", channel="TEST"} 50
+mq_channel_status{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 3
+mq_channel_buffers{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", indicator="buffers_received", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 7216
+mq_channel_buffers{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", indicator="buffers_sent", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 7215
+mq_channel_bytes{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", indicator="bytes_received", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 4894300
+mq_channel_bytes{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", indicator="bytes_sent", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 949752
+mq_channel_lmsg{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 1584630901
+mq_channel_msgs{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 1510
+mq_channel_batches{qmname="TEST", conname="127.0.0.1", substate="RECEIVE", xmitq="", chltype="SVRCONN", chstada="2020-03-19", chstati="18.00.00", rqmname="", jobname="000010EC00000007", channel="ADMIN.SVRCONN"} 0\n'''
         self.assertEqual(check_data, get_mq_channels_metrics(input_data, mqm))
+
 
 
 if __name__ == '__main__':
